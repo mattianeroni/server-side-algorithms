c5dcd218fcdccf8aa4b6fb0d701240da945622f8
mattia.neroni <mattia.neroni@unipr.it>
2022-05-13 09:48:32 +0200

    start

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ssa',
+    version='0.0.1',
+    description='A platform where scientists can upload algorithms by earning from them, and developers can find the algorithms they need.',
+    author='Mattia Neroni, Ph.D, Eng.',
+    author_email='mneroni@uoc.edu',
+    url='https://github.com/mattianeroni/server-side-algorithms',
+    package_dir = {
+        'ssa': 'ssa'
+    },
+    packages=[
+        'ssa'
+    ],
+    python_requires='>=3.8',
+    classifiers=[
+        "Development Status :: 3 - Alpha"
+    ],
+    install_requires=[
+        'numpy>=1.2.0',
+        'pandas',
+    ],
+)
