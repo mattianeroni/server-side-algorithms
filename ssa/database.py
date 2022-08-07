51f48091564138e6517198a49bc43a2eef3e75eb
mattianeroni <54416801+mattianeroni@users.noreply.github.com>
2022-08-07 10:32:08 +0200

    Update database.py

--- ssa/database.py
+++ ssa/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 
 
 #SQLALCHEMY_DATABASE_URL = "sqlite+aiosqlite:///./ssadb.db"
-SQLALCHEMY_DATABASE_URL = "postgresql+asyncpg://ssa_user:Budinosauro11@localhost/ssadb"
+SQLALCHEMY_DATABASE_URL = "postgresql+asyncpg://ssa_user:password@localhost/ssadb"
 
 engine = create_async_engine(SQLALCHEMY_DATABASE_URL, future=False, echo=False) #connect_args={"check_same_thread": False} 
 async_session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
